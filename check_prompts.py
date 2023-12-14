e600a8bdae1e94c972c5c810bc0f1a02a4b75f82
Federico Torrielli <evilscript@protonmail.com>
2023-12-14 14:21:14 +0100

    refactor: check_prompts.py

--- check_prompts.py
+++ check_prompts.py
@@ -1,8 +1,5 @@
-import os
-
-
 def get_num_rows(file):
-    with open(file, 'r') as reader:
+    with open(file, "r") as reader:
         num_rows = 0
         for _ in reader:
             num_rows += 1
@@ -14,16 +11,16 @@ def check_files(f1, f2):
     n_rows_1 = get_num_rows(f1)
     n_rows_2 = get_num_rows(f2)
 
-    assert n_rows_1 == n_rows_2, f'{n_rows_1} != {n_rows_2}'
+    assert n_rows_1 == n_rows_2, f"{n_rows_1} != {n_rows_2}"
 
 
-if __name__ == '__main__':
-
-    prompt_list = ['./prompts/zero_shot_prompt_v1.json',
-                   './prompts/zero_shot_prompt_v2.json',
-                   './prompts/zero_shot_prompt_v3.json',
-                   './prompts/zero_shot_prompt_v4.json'
-                   ]
+if __name__ == "__main__":
+    prompt_list = [
+        "./prompts/zero_shot_prompt_v1.json",
+        "./prompts/zero_shot_prompt_v2.json",
+        "./prompts/zero_shot_prompt_v3.json",
+        "./prompts/zero_shot_prompt_v4.json",
+    ]
 
     for p1 in prompt_list:
         for p2 in prompt_list:
