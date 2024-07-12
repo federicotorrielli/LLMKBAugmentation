9968bb6769e2ccee86039742c4e7c5b4e80b52e1
Federico Torrielli <evilscript@protonmail.com>
2024-07-12 15:46:26 +0200

    I have overoptimized the code

--- inference.py
+++ inference.py
@@ -1,5 +1,7 @@
 import gc
 import os
+import re
+
 import requests
 import torch
 import ujson
@@ -7,116 +9,98 @@ from tqdm import tqdm
 from vllm import LLM, SamplingParams
 from vllm.distributed.parallel_state import destroy_model_parallel
 
-# Define the common model names for all tasks
-model_names = [
-    # "mistralai/Mistral-7B-Instruct-v0.3",
-    # "TheBloke/Yi-34B-AWQ",
-    # "TheBloke/tulu-2-70B-AWQ",
-    # "TheBloke/Aetheria-L2-70B-AWQ",
-    "microsoft/Phi-3-medium-4k-instruct"
-    # "google/gemma-2-27b-it",
+MODEL_NAMES = [
+    "TechxGenus/Meta-Llama-3-70B-AWQ",  # foundational | 70B | AWQ
+    "TechxGenus/Meta-Llama-3-70B-Instruct-AWQ",  # instruct | 70B | AWQ
+    "Sao10K/L3-8B-Stheno-v3.2",  # storytelling/instruct | 8B | unquantized
+    "microsoft/Phi-3-medium-4k-instruct",  # instruct | 14B | unquantized
+    "mistralai/Mistral-7B-Instruct-v0.3",  # instruct | 7B | unquantized
+    "google/gemma-2-27b-it"  # instruct | 27B | unquantized - need to be run with VLLM_ATTENTION_BACKEND=FLASHINFER
 ]
 
-# Define input files for different tasks
-tasks = {
+TASKS = {
     "conceptnet": [
-        "conceptnet_zero_shot_prompt_v1.json",
-        "conceptnet_zero_shot_prompt_v2.json",
-        "conceptnet_zero_shot_prompt_v3.json",
-        "conceptnet_one_shot_prompt_v1.json",
+        "conceptnet_zero_shot_prompt_usedfor_v1.json",
+        "conceptnet_zero_shot_prompt_usedfor_v2.json",
+        "conceptnet_zero_shot_prompt_usedfor_v3.json",
+        "conceptnet_one_shot_prompt_usedfor_v1.json",
+        "conceptnet_zero_shot_prompt_relatedto_v1.json",
+        "conceptnet_zero_shot_prompt_relatedto_v2.json",
+        "conceptnet_zero_shot_prompt_relatedto_v3.json",
+        "conceptnet_one_shot_prompt_relatedto_v1.json",
     ],
     "multialignet": [
-        "multialignet_adjectives_oneshot_v1.json",
-        "multialignet_adjectives_oneshot_v2.json",
-        "multialignet_adjectives_oneshot_v3.json",
-        "multialignet_adjectives_zeroshot_v1.json",
-        "multialignet_adjectives_zeroshot_v2.json",
-        "multialignet_adjectives_zeroshot_v3.json",
-        "multialignet_nouns_oneshot_v1.json",
-        "multialignet_nouns_oneshot_v2.json",
-        "multialignet_nouns_oneshot_v3.json",
-        "multialignet_nouns_zeroshot_v1.json",
-        "multialignet_nouns_zeroshot_v2.json",
-        "multialignet_nouns_zeroshot_v3.json",
-        "multialignet_verbs_oneshot_v1.json",
-        "multialignet_verbs_oneshot_v2.json",
-        "multialignet_verbs_oneshot_v3.json",
-        "multialignet_verbs_zeroshot_v1.json",
-        "multialignet_verbs_zeroshot_v2.json",
-        "multialignet_verbs_zeroshot_v3.json",
+        f"multialignet_{word_type}_{shot_type}_v{v}.json"
+        for word_type in ["adjectives", "nouns", "verbs"]
+        for shot_type in ["oneshot", "zeroshot"]
+        for v in range(1, 4)
     ],
     "semagram": [
-        "zero_shot_prompt_v1.json",
-        "zero_shot_prompt_v2.json",
-        "zero_shot_prompt_v3.json",
-        "zero_shot_prompt_v4.json",
-        "one_shot_prompt_v1.json",
+        f"{'zero' if i < 4 else 'one'}_shot_prompt_v{i}.json" for i in range(1, 6)
     ],
 }
 
-sampling_params = SamplingParams(top_p=0.95, temperature=0.4, max_tokens=100)
-t = sampling_params.temperature
-top_p = sampling_params.top_p
-max_new_tokens = sampling_params.max_tokens
+SAMPLING_PARAMS = SamplingParams(top_p=0.95, temperature=0.4, max_tokens=100)
 
 
-def get_number_prompts(file_name):
+def get_modified_prompt(prompt, model_name):
+    if "gemma" in model_name.lower() and "-it" in model_name.lower():
+        return (
+            f"<bos><start_of_turn>user\n{prompt}<end_of_turn>\n<start_of_turn>model\n"
+        )
+    elif "phi" in model_name.lower():
+        return f"<|user|>\n{prompt}<|end|>\n<|assistant|>\n"
+    elif ("meta" in model_name.lower() and "instruct" in model_name.lower()) or "stheno" in model_name.lower():
+        return f"<|start_header_id|>user<|end_header_id|>\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    return prompt
+
+
+def process_file(file_name, task_type, model_name):
     with open(file_name, "r") as reader:
-        all_data = ujson.load(reader)
-        return len(all_data)
-
-
-def prompt_generator(file_name, task_type, model_name):
-    with open(file_name, "r") as reader:
-        all_data = ujson.load(reader)
-        for data in all_data:
-            if "gemma" in model_name.lower() and "-it" in model_name.lower():
-                modified_prompt = f"<bos><start_of_turn>user\n{data['prompt']}<end_of_turn>\n<start_of_turn>model\n"
-            elif "phi" in model_name.lower():
-                modified_prompt = f"<|user|>\n{data['prompt']}<|end|>\n<|assistant|>\n"
-            else:
-                modified_prompt = data["prompt"]
-
+        data = ujson.load(reader)
+        for item in data:
+            prompt = get_modified_prompt(item["prompt"], model_name)
             if task_type == "conceptnet":
-                yield data["concept"], modified_prompt
+                yield item["concept"], prompt
             elif task_type == "multialignet":
-                yield data["count"], data["lemmas"], data["wordnet_id"], modified_prompt
+                yield item["count"], item["lemmas"], item["wordnet_id"], prompt
             elif task_type == "semagram":
-                yield data["cat"], data["slot"], data["value"], modified_prompt
+                yield item["cat"], item["slot"], item["value"], prompt
 
 
-def get_only_prompts(file_name, model_name):
+def get_prompts(file_name, model_name):
     with open(file_name, "r") as reader:
-        all_data = ujson.load(reader)
-        if "gemma" in model_name.lower() and "-it" in model_name.lower():
-            return [
-                f"<bos><start_of_turn>user\n{data['prompt']}<end_of_turn>\n<start_of_turn>model\n"
-                for data in all_data
-            ]
-        if "phi" in model_name.lower():
-            return [
-                f"<|user|>\n{data['prompt']}<|end|>\n<|assistant|>\n"
-                for data in all_data
-            ]
-        return [data["prompt"] for data in all_data]
-
-
-def get_simple_model_name(m_name):
-    return m_name.split("/")[-1] if "/" in m_name else m_name
+        return [
+            get_modified_prompt(item["prompt"], model_name)
+            for item in ujson.load(reader)
+        ]
 
 
 def download_file(url, file_name):
     r = requests.get(url, allow_redirects=True)
-    open(file_name, "wb").write(r.content)
+    with open(file_name, "wb") as f:
+        f.write(r.content)
+
+
+def clean_output(text):
+    # Remove everything between <|...|>
+    text = re.sub(r"<\|.*?\|>", "", text)
+
+    # Remove other specific patterns
+    patterns = ["<bos>", "<eos>", "[INST]", "[/INST]", "\n"]
+    for pattern in patterns:
+        text = text.replace(pattern, "")
+
+    return text.strip()
 
 
 def run_inference(model_names, tasks):
     downloaded_files = []
 
     for model_name in model_names:
-        f_m_name = get_simple_model_name(model_name)
         print(f"Running model {model_name}")
-
+        if "gemma" in model_name.lower():
+            os.environ["VLLM_ATTENTION_BACKEND"] = "FLASHINFER"
         model = LLM(
             model_name,
             trust_remote_code=True,
@@ -133,63 +117,43 @@ def run_inference(model_names, tasks):
                     )
                     downloaded_files.append(file_name)
 
-                num_prompts = get_number_prompts(file_name)
-
-                output_dir = os.path.join(task_type, f_m_name)
+                output_dir = os.path.join(task_type, model_name.split("/")[-1])
                 os.makedirs(output_dir, exist_ok=True)
-                file_output = os.path.join(
+                output_file = os.path.join(
                     output_dir,
-                    f"t_{t}__top_p_{top_p}__max_new_tokens_{max_new_tokens}__file_{file_name.replace('.json', '')}.jsonl",
+                    f"t_{SAMPLING_PARAMS.temperature}__top_p_{SAMPLING_PARAMS.top_p}__max_new_tokens_{SAMPLING_PARAMS.max_tokens}__file_{file_name.replace('.json', '')}.jsonl",
                 )
 
-                print(
-                    f"Processing {file_name} with {num_prompts} prompts for task {task_type}"
-                )
+                print(f"Processing {file_name} for task {task_type}")
 
-                with open(file_output, "w") as writer:
-                    generator = prompt_generator(file_name, task_type, model_name)
-                    full_list_prompts = get_only_prompts(file_name, model_name)
-                    model_outputs = model.generate(
-                        full_list_prompts, sampling_params=sampling_params
-                    )
-                    outputs_dict = {
-                        output.prompt: output.outputs[0]
-                        .text.replace("<bos>", "")
-                        .replace("<start_of_turn>", "")
-                        .replace("<end_of_turn>", "")
-                        .replace("<eos>", "")
-                        .replace("[INST]", "")
-                        .replace("[/INST]", "")
-                        .replace("<|user|>", "")
-                        .replace("<|assistant|>", "")
-                        .replace("<|end|>", "")
-                        .replace("\n", "")
-                        .strip()
-                        for output in model_outputs
+                with open(output_file, "w") as writer:
+                    prompts = get_prompts(file_name, model_name)
+                    outputs = model.generate(prompts, sampling_params=SAMPLING_PARAMS)
+                    output_dict = {
+                        output.prompt: clean_output(output.outputs[0].text)
+                        for output in outputs
                     }
 
-                    for prompts in tqdm(generator, total=num_prompts):
-                        prompt = prompts[-1]
-                        model_output = outputs_dict[prompt]
-                        result = {"result": model_output, "prompt": prompt}
+                    for item in tqdm(
+                            process_file(file_name, task_type, model_name),
+                            total=len(prompts),
+                    ):
+                        prompt = item[-1]
+                        result = {"result": output_dict[prompt], "prompt": prompt}
 
                         if task_type == "conceptnet":
-                            result.update({"concept": prompts[0]})
+                            result["concept"] = item[0]
                         elif task_type == "multialignet":
                             result.update(
                                 {
-                                    "count": prompts[0],
-                                    "lemmas": prompts[1],
-                                    "wordnet_id": prompts[2],
+                                    "count": item[0],
+                                    "lemmas": item[1],
+                                    "wordnet_id": item[2],
                                 }
                             )
                         elif task_type == "semagram":
                             result.update(
-                                {
-                                    "cat": prompts[0],
-                                    "slot": prompts[1],
-                                    "value": prompts[2],
-                                }
+                                {"cat": item[0], "slot": item[1], "value": item[2]}
                             )
 
                         writer.write(ujson.dumps(result) + "\n")
@@ -201,10 +165,8 @@ def run_inference(model_names, tasks):
             torch.cuda.empty_cache()
             torch.distributed.destroy_process_group()
 
-    # Delete downloaded files
     for file in downloaded_files:
         os.remove(file)
 
 
-# Run inference for each model and each task
-run_inference(model_names, tasks)
+run_inference(MODEL_NAMES, TASKS)
